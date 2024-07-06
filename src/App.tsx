4eb6a0fce466f046310e43337cad6cfdf67a207d
Lycoriste <steventeng03@gmail.com>
2024-07-06 22:53:49 +0700

    Added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { getCurrentTabUrl } from "./utils/getCurrentTabUrl";
 import { useEffect, useState } from "react";
 import { scrapeRecipePage, scrapeIngredients } from "./services/scraper";
 
+import { Header } from "./components/Header.tsx";
+import { Button } from "./components/Button.tsx";
+
 export default function App() {
   const [currentTabUrl, setCurrentTabUrl] = useState<string>("");
   const [html, setHtml] = useState<string>("");
@@ -35,11 +38,12 @@ export default function App() {
   }, [html]);
 
   return (
-    <div>
-      <h1> hello</h1>
+    <div className="w-60 h-80 flex flex-col">
+      <Header />
       {currentTabUrl && <p>link: {currentTabUrl}</p>}
       {/* {html && <p>html: {html}</p>} */}
       {ingredients && <p>ingredients: {ingredients}</p>}
-    </div>
+      <Button>Calculate</Button>
+    </div >
   );
 }
