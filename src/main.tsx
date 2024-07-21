50b52868af531168513e31ec47e4d3d09185db05
Lycoriste <steventeng03@gmail.com>
2024-07-21 12:41:06 -0700

    Added Button and Page2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Page2 from "./components/Page2.tsx"
+import Page2 from "./pages/Page2.tsx"
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
