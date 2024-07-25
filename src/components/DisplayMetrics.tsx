ab79ca3fbaa19cbae0304d33c546bf44ca2134ec
cnrbd <puong23@ssis.edu.vn>
2024-07-25 21:49:15 +0700

    starting on display metrics

---
+++ src/components/DisplayMetrics.tsx
@@ -0,0 +1,5 @@
+// takes in props of the ingredients and the currentPrompts object
+// useEffect for openai api response
+// imports are openai api functions, diplaymetricshelpers functions
+
+export default function DisplayMetrics() {}
