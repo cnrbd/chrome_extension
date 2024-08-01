b7e44e573494c2bca6d5d85bead95fbe58e2c8ed
cnrbd <puong23@ssis.edu.vn>
2024-08-02 05:23:26 +0700

    implement graph

--- src/openai/test.ts
+++ src/openai/test.ts
@@ -27,10 +27,11 @@ export const stats = async (
           role: "user",
           content: `Given the following ingredients string from a recipe: ${ingredients}, answer the nutritional value questions based on the keys in the provided object: ${JSON.stringify(
             currentPrompts
-          )}. Return the result as an object with the same keys as the input object, where each key's value is an array of strings that correspond to the answers for that key.`,
+          )}. Return the result as an object with the same keys as the input object, where each key's value is an array of strings that correspond to the answers for that key. Rememeber to close and open the object with curly braces,
+          Express calories in kcal, sodium in mg, and all other nutrients in grams.`,
         },
       ],
-      max_tokens: 200,
+      max_tokens: 1000,
       temperature: 1,
     });
     if (response && response.choices && response.choices.length > 0) {
