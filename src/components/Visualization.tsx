a1ac25bf7d1b0c109fe02ce2d30c0dfc404985aa
cnrbd <puong23@ssis.edu.vn>
2024-08-04 18:26:37 +0700

    add graph

--- src/components/Visualization.tsx
+++ src/components/Visualization.tsx
@@ -1,13 +1,15 @@
 import Plot from "react-plotly.js";
 import { useState, useEffect } from "react";
 import { stringObjectToJSON } from "../utils/displayMetricsHelpers";
-
+import {
+  extractStats,
+  calculatePercentage,
+} from "../utils/visualizationHelpers";
 type GraphProps = {
   aiResponse: string;
 };
 
 export default function Graph({ aiResponse }: GraphProps) {
-  const r = /\d+/;
   const [statsArray, setStatsArray] = useState<number[]>([]);
   const [responseJSON, setResponseJSON] = useState<{ [key: string]: string[] }>(
     {}
@@ -26,35 +28,14 @@ export default function Graph({ aiResponse }: GraphProps) {
     }
   }, [aiResponse]);
 
-  const extractStats = (aiResponse: { [key: string]: string[] }) => {
-    const tempArray: number[] = [];
-    for (const key in aiResponse) {
-      const keyStats = aiResponse[key];
-      if (keyStats) {
-        const statValue = keyStats[0].match(r);
-        if (statValue) {
-          tempArray.push(Number(statValue[0]));
-        }
-      }
-    }
-    setStatsArray(tempArray);
-  };
-
   useEffect(() => {
     if (aiResponse && responseJSON) {
-      extractStats(responseJSON);
+      const tempArray = extractStats(responseJSON);
+      setStatsArray(tempArray);
       console.log(statsArray);
     }
   }, [responseJSON]);
 
-  //   const recommmnededAmountsPerMeal = {
-  //     calories: 600,
-  //     carbohydrates: 50,
-  //     protein: 20,
-  //     fat: 20,
-  //     fiber: 10,
-  //     sodium: 500,
-  //   };
   return (
     aiResponse &&
     responseJSON && (
@@ -62,7 +43,7 @@ export default function Graph({ aiResponse }: GraphProps) {
         data={[
           {
             x: keys,
-            y: statsArray,
+            y: calculatePercentage(statsArray, keys),
             type: "bar",
           },
         ]}
