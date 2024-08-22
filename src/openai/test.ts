ddbacfefd0ff018b5cdad0ce31f4360d7f8b578a
cnrbd <puong23@ssis.edu.vn>
2024-08-22 13:13:21 +0700

    test

--- src/openai/test.ts
+++ src/openai/test.ts
@@ -4,7 +4,7 @@ import { MetricKeys } from "../utils/displayMetricsHelpers";
 const openAIApiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 console.log(openAIApiKey);
 
-const serverUrl = "https://chrome-extension-teal.vercel.app/openai";
+const serverUrl = "https://chrome-extension-teal.vercel.app/test";
 
 // const openai = new OpenAI({
 //   apiKey: openAIApiKey,
