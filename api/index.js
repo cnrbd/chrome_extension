b2ef0f38304e575e3d6486d89528d1022be08e25
cnrbd <puong23@ssis.edu.vn>
2024-08-24 15:15:28 +0700

    change prompt

--- api/index.js
+++ api/index.js
@@ -133,7 +133,8 @@ app.post("/test", async (req, res) => {
           role: "user",
           content: `Given the following ingredients string from a recipe: ${ingredients}, answer the nutritional value questions based on the keys in the provided object: ${JSON.stringify(
             currentPrompts
-          )}. Return the result as an object with the same keys as the input object, where each key's value is an array of strings that correspond to the answers for that key. Rememeber to close and open the object with curly braces, Express calories in kcal, sodium in mg, and all other nutrients in grams. Write other answers in full sentences.`,
+          )}. Return the result as an object with the same keys as the input object, where each key's value is an array of strings that correspond to the answers for that key. Rememeber to close and open the object with curly braces,
+Express calories in kcal, sodium in mg, and all other nutrients in grams. Write other answers in full sentences.`,
         },
       ],
       model: "llama-guard-3-8b",
