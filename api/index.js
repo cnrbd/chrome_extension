8795bfb120b67073d994df18570c7c219bfe9260
cnrbd <puong23@ssis.edu.vn>
2024-08-24 15:20:46 +0700

    change model

--- api/index.js
+++ api/index.js
@@ -137,7 +137,7 @@ app.post("/test", async (req, res) => {
 Express calories in kcal, sodium in mg, and all other nutrients in grams. Write other answers in full sentences.`,
         },
       ],
-      model: "lllama3-8b-8192",
+      model: "mixtral-8x7b-32768",
       max_tokens: 1000,
       temperature: 1,
     });
